f58566382294c83730e8bd9bba52b1a23bf9b39b
Daniel Gutierres <daniel113388@gmail.com>
2025-07-21 23:59:07 -0300

    feat: initialize Angular project with basic structure and configurations
    - Added environment configuration files for production and development.
    - Created main application files including index.html, main.ts, and polyfills.ts.
    - Implemented basic styles in styles.css for consistent UI.
    - Included favicon and moments image assets.
    - Set up TypeScript configuration files for application and testing.
    - Established testing environment with necessary imports and configurations.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NewMomentsComponent } from './components/pages/new-moments/new-moments.component';
+import { NgModule} from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { AboutComponent } from './components/pages/about/about.component';
+
+const routes: Routes = [
+   {path: '', component: HomeComponent},  //'' significa Home 
+   {path: 'about', component: AboutComponent},
+   {path: 'moments/new', component: NewMomentsComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
