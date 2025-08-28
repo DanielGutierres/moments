d59aaa95dcf48b1c89cde5b2287a33fd0f93f081
Daniel Gutierres <daniel113388@gmail.com>
2025-08-27 23:31:53 -0300

    fix: update moment form component for improved file input handling and code consistency

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -4,16 +4,15 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-moment-form',
   templateUrl: './moment-form.component.html',
-  styleUrls: ['./moment-form.component.css']
+  styleUrls: ['./moment-form.component.css'],
 })
 export class MomentFormComponent implements OnInit {
-[x: string]: any;
-  @Input() btnText!: string
+  [x: string]: any;
+  @Input() btnText!: string;
 
-  momentForm!: FormGroup
+  momentForm!: FormGroup;
 
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     this.momentForm = new FormGroup({
@@ -32,12 +31,16 @@ export class MomentFormComponent implements OnInit {
     return this.momentForm.get('description')!;
   }
 
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    if (file) {
+      this.momentForm.patchValue({ image: file });
+    }
+  }
   submit() {
-
-    if(this.momentForm.invalid) {
-      return
+    if (this.momentForm.invalid) {
+      return;
     }
     console.log('Form submitted');
   }
-
 }
